aa34091d57d0b68ba0752e6f4f74c559fa6e1965
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-05-21 16:53:17 +0700

    BaiThi

---
+++ Bai_Thi/Table.sql
@@ -0,0 +1,9 @@
+create table Student (
+                         studenID int primary key auto_increment,
+                         studenName varchar(50),
+                         address varchar(150),
+                         phone varchar(20)
+);
+insert into students values
+(B01010,"Nguyen Tuan Anh","nta@gmail.com", 0862473525),
+(B10394, " Hoang","Hoa binh", 09654657);
